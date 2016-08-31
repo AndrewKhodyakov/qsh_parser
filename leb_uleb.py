dff36bf50da03618c3c1275252c08fc4a2babc09
ajet <korol.pod.goroy@gmail.com>
2016-08-31 12:41:42 +0300

    Добавил изменения в перекодировку в бинарный вид

--- leb_uleb.py
+++ leb_uleb.py
@@ -4,44 +4,45 @@
 """
     Попытки_написать_кодировик_декодировщик_leb__uleb
 """
+from types import GeneratorType
 #===============================================================================
-def shifting(integer, shift):
-    """
-    Сдвиг на лево
-    integer: целое число для проведения одного шага вычислений
-    shift: коллличесвто бит на которые неодбходимо производить сдвиг
-    """
-
-    int_to_str_of_bits = lambda integer: bin(integer).split('b')[1]
-    zero_padding = lambda str_of_bites, shift:\
-        '0' * ((shift + 1) - len(str_of_bites)) + str_of_bites
-    unity_padding = lambda seven_bits: '1' + seven_bits
-    get_seveb_bits = lambda str_of_bites: str_of_bites[-7:]
-    
-
-    result = []
-    integer = integer
-    shift = shift
-    while True:
-        rest = bin(integer >> shift)
-        if rest == '0b0': #exit
-            result.append(
-                zero_padding(
-                    int_to_str_of_bits(integer), shift
-                )
-            )
-            return result
-            break
-
-        else: #go on
-            result.append(
-                unity_padding(
-                    get_seveb_bits(
-                        int_to_str_of_bits(integer)
-                    )
-                )
-            )
-            integer = int(rest, 2)
+#def shifting(integer, shift):
+#    """
+#    Сдвиг на лево
+#    integer: целое число для проведения одного шага вычислений
+#    shift: коллличесвто бит на которые неодбходимо производить сдвиг
+#    """
+#
+#    int_to_str_of_bits = lambda integer: bin(integer).split('b')[1]
+#    zero_padding = lambda str_of_bites, shift:\
+#        '0' * ((shift + 1) - len(str_of_bites)) + str_of_bites
+#    unity_padding = lambda seven_bits: '1' + seven_bits
+#    get_seveb_bits = lambda str_of_bites: str_of_bites[-7:]
+#    
+#
+#    result = []
+#    integer = integer
+#    shift = shift
+#    while True:
+#        rest = bin(integer >> shift)
+#        if rest == '0b0': #exit
+#            result.append(
+#                zero_padding(
+#                    int_to_str_of_bits(integer), shift
+#                )
+#            )
+#            return result
+#            break
+#
+#        else: #go on
+#            result.append(
+#                unity_padding(
+#                    get_seveb_bits(
+#                        int_to_str_of_bits(integer)
+#                    )
+#                )
+#            )
+#            integer = int(rest, 2)
 
 int_to_str_of_bits = lambda integer: bin(integer).split('b')[1]
 zero_padding = lambda str_of_bites, shift:\
@@ -54,6 +55,9 @@ class ULeb128:
     """
     def __init__(self):
         """
+        self.__shift: колличество бит для кодирования (7 по-умолчанию)
+        self.__input: число поданное на вход
+        self.__output: результат кодирвания в uleb128
         """
         self.__shift = 7
         self.__input = None
@@ -61,6 +65,8 @@ class ULeb128:
 
     def encoding(self, integer):
         """
+        Кодирование числе в формат uleb128
+        integer: - целое число для кодирования
         """
         if not isinstance(integer, int):
             raise TypeError('Type encoding input is not int')
@@ -86,26 +92,30 @@ class ULeb128:
                     )
                 )
                 self.__input = int(rest, 2)
+
+        self.__output = [hex(int(i, 2)) for i in self.__output]
           
     @property
-    def result(self, encode=False, decode=False):
+    def result(self):
         """
         Return result
-        encode: default False, encoding result
-        decode: default False, decoding result
         """
-        out = None
-        if encode:
-        if decode:
-        if (encode is True) & (decode is True):
-            raise ValueError('Specify only one ')
-        return out
-        
+        return self.__output
+            
 
-    def dencoding(self, byte):
+    def dencoding(self, byte_stream):
         """
+        byte_stream: byte generator (генератор байт, отадет их по одной)
         """
-        pass
+        if not isinstance(byte_stream, GeneratorType):
+            raise TypeError('Type decoding input is GeneratorType')
+
+        #они записаны задом на перед, поэтому надо читать каждый до тех пор
+        #пока не появится число начинающиеся на 0
+        while True:
+            
+            if higt_bit == '0':
+                break
 
     def __str_(self):
         """
